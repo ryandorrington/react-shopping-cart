d5bd8bbaf63f210bfdb53bd5ba44b9015bb79359
Ryan Dorrington <dorringtonryan@gmail.com>
2021-12-27 11:53:59 +0800

    add qty button functionality

--- src/Cart.js
+++ src/Cart.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import CartItem from "./CartItem";
 function Cart({ initialItems }) {
+  const [items, setItems] = useState(initialItems);
+
+  const updateQty = (id, newQty) => {
+    const newItems = items.map((item) => {
+      if (item.id === id) {
+        return { ...item, qty: newQty };
+      }
+      return item;
+    });
+    setItems(newItems);
+  };
+
+  const grandTotal = items
+    .reduce((total, item) => total + item.qty * item.price, 0)
+    .toFixed(2);
   return (
     <div className="Cart">
       <h1>cart</h1>
       <div className="Cart-items">
-        {initialItems.map((item) => (
-          <CartItem {...item} />
+        {items.map((item) => (
+          <CartItem key={item.id} updateQty={updateQty} {...item} />
         ))}
       </div>
+      <h2>Total: ${grandTotal}</h2>
     </div>
   );
 }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,9 +1,27 @@
 import React from "react";
 
-function CartItem({ id, name, price, qty }) {
+function CartItem({ id, name, price, qty, updateQty }) {
   return (
     <div className="CartItem">
       <div>{name}</div>
+      <div>{price}</div>
+      <div>
+        <button
+          onClick={() => {
+            updateQty(id, qty - 1);
+          }}
+        >
+          -
+        </button>
+        {qty}
+        <button
+          onClick={() => {
+            updateQty(id, qty + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 }
